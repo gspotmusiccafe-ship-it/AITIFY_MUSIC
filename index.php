35daefec3fa912a579a77d578ebf615d6f2ed631
gspotmusiccafe-ship-it <gspotmusiccafe@gmail.com>
2026-06-06 00:09:07 -0500

    Update index.php

--- index.php
+++ index.php
@@ -6,9 +6,9 @@ if (version_compare(PHP_VERSION, '8.1') === -1) {
     exit('You need at least PHP ' . '8.1' . ' to install this application.');
 }
 
-require __DIR__ . '/../bootstrap/autoload.php';
+require __DIR__ . '/vendor/autoload.php';
 
-$app = require_once __DIR__ . '/../bootstrap/app.php';
+$app = require_once __DIR__ . '/bootstrap/app.php';
 
 if (!file_exists(__DIR__ . '/../.env')) {
     $app->loadEnvironmentFrom('env.example');
